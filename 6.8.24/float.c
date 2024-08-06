5dad35ec635ccb6c0295c7696d983fdcc25df975
Giridharan <151344363+MGDharan@users.noreply.github.com>
2024-08-06 21:43:51 +0530

    Create float.c

---
+++ 6.8.24/float.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+float multiply(float a, float b) 
+{ 
+return a * b; 
+} 
+int main() 
+{ 
+	float A = 2.12, B = 3.88, product; 
+	product = multiply(A, B); 
+	printf("Product of entered numbers is:%.3f", product); 
+
+	return 0; 
+}
