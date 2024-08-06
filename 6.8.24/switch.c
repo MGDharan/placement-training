018bddb218c381c8e2b3f7b344adffe3b6f093fb
Giridharan <151344363+MGDharan@users.noreply.github.com>
2024-08-06 21:52:57 +0530

    Create switch.c

---
+++ 6.8.24/switch.c
@@ -0,0 +1,12 @@
+int x = 4;
+switch (x) {
+    case 1:
+        printf("Choice is 1");
+        break;
+    case 2:
+        printf("Choice is 2");
+        break;
+    case 3:
+        printf("Choice is 3");
+        break;
+    }
