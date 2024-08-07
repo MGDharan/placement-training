55b6dd9a934bda29245022cde25c1daa95138070
Giridharan <151344363+MGDharan@users.noreply.github.com>
2024-08-07 22:40:34 +0530

    Create pass.py

---
+++ 7.8.24/pass.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+password = generate_password(12)
+print("Generated Password:", password)
