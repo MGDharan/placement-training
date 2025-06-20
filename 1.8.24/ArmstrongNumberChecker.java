3b892cf0d1cec05648077fcfa5368057cb478cc3
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-20 19:47:06 +0530

    cHVibGljIGNsYXNzICBBcm1zdHJvbmdOdW1iZXJDaGVja2VyIHsKCiAgICBwdWJsaWMgc3RhdGljIGJvb2xlYW4gaXNBcm1zdHJvbmdOdW1iZXIoaW50IG51bSkgewogICAgICAgIGludCBvcmlnaW5hbE51bWJlciA9IG51bTsKICAgICAgICBpbnQgbnVtRGlnaXRzID0gU3RyaW5nLnZhbHVlT2YobnVtKS5sZW5ndGgoKTsKICAgICAgICBpbnQgc3VtID0gMDsKICAgICAgICB3aGlsZSAobnVtID4gMCkgewogICAgICAgICAgICBpbnQgZGlnaXQgPSBudW0gJSAxMDsKICAgICAgICAgICAgc3VtICs9IE1hdGgucG93KGRpZ2l0LCBudW1EaWdpdHMpOwogICAgICAgICAgICBudW0gLz0gMTA7CiAgICAgICAgfQogICAgICAgIHJldHVybiBzdW0gPT0gb3JpZ2luYWxOdW1iZXI7CiAgICB9CgogICAgcHVibGljIHN0YXRpYyB2b2lkIG1haW4oU3RyaW5nW10gYXJncykgewogICAgICAgIGphdmEudXRpbC5TY2FubmVyIHNjYW5uZXIgPSBuZXcgamF2YS51dGlsLlNjYW5uZXIoU3lzdGVtLmluKTsKICAgICAgICBTeXN0ZW0ub3V0LnByaW50KCJFbnRlciBhIG51bWJlcjogIik7CiAgICAgICAgaW50IG51bWJlciA9IHNjYW5uZXIubmV4dEludCgpOwogICAgICAgIGlmIChpc0FybXN0cm9uZ051bWJlcihudW1iZXIpKSB7CiAgICAgICAgICAgIFN5c3RlbS5vdXQucHJpbnRsbihudW1iZXIgKyAiIGlzIGFuIEFybXN0cm9uZyBudW1iZXIuIik7CiAgICAgICAgfSBlbHNlIHsKICAgICAgICAgICAgU3lzdGVtLm91dC5wcmludGxuKG51bWJlciArICIgaXMgbm90IGFuIEFybXN0cm9uZyBudW1iZXIuIik7CiAgICAgICAgfQogICAgICAgIHNjYW5uZXIuY2xvc2UoKTsKICAgIH0KfQ==

---
+++ 1.8.24/ArmstrongNumberChecker.java
@@ -0,0 +1,26 @@
+public class  ArmstrongNumberChecker {
+
+    public static boolean isArmstrongNumber(int num) {
+        int originalNumber = num;
+        int numDigits = String.valueOf(num).length();
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, numDigits);
+            num /= 10;
+        }
+        return sum == originalNumber;
+    }
+
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (isArmstrongNumber(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+        scanner.close();
+    }
+}
