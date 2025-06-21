64d1fa47541e8abd1ae4f83f1bd58094b3d70c78
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-21 18:32:28 +0530

    cHVibGljIGNsYXNzIFByaW1lTnVtYmVyQ2hlY2tlciB7CgogICAgcHVibGljIHN0YXRpYyBib29sZWFuIGlzUHJpbWUoaW50IG4pIHsKICAgICAgICBpZiAobiA8PSAxKSB7CiAgICAgICAgICAgIHJldHVybiBmYWxzZTsKICAgICAgICB9CiAgICAgICAgZm9yIChpbnQgaSA9IDI7IGkgPD0gTWF0aC5zcXJ0KG4pOyBpKyspIHsKICAgICAgICAgICAgaWYgKG4gJSBpID09IDApIHsKICAgICAgICAgICAgICAgIHJldHVybiBmYWxzZTsKICAgICAgICAgICAgfQogICAgICAgIH0KICAgICAgICByZXR1cm4gdHJ1ZTsKICAgIH0KCiAgICBwdWJsaWMgc3RhdGljIHZvaWQgbWFpbihTdHJpbmdbXSBhcmdzKSB7CiAgICAgICAgaW50IG51bSA9IDI5OwogICAgICAgIGlmIChpc1ByaW1lKG51bSkpIHsKICAgICAgICAgICAgU3lzdGVtLm91dC5wcmludGxuKG51bSArICIgaXMgYSBwcmltZSBudW1iZXIiKTsKICAgICAgICB9IGVsc2UgewogICAgICAgICAgICBTeXN0ZW0ub3V0LnByaW50bG4obnVtICsgIiBpcyBub3QgYSBwcmltZSBudW1iZXIiKTsKICAgICAgICB9CiAgICB9Cn0=

---
+++ 21.06.2025/PrimeNumberChecker.java
@@ -0,0 +1,23 @@
+public class PrimeNumberChecker {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 29;
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number");
+        } else {
+            System.out.println(num + " is not a prime number");
+        }
+    }
+}
