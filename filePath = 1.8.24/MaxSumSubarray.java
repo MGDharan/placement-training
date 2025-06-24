d1936564f527f23fb15d5acf6579307523765003
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-24 19:48:20 +0530

    Upload MaxSumSubarray.java

---
+++ filePath = 1.8.24/MaxSumSubarray.java
@@ -0,0 +1,15 @@
+public class  MaxSumSubarray{
+    public static int maxSubArraySum(int[] nums){
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+    public static void main(String[] args){
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArraySum(nums));
+    }
+}
