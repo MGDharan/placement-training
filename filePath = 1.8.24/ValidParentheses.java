4672132baab7c6f120a4d00d79acbbfb735721c7
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-24 19:50:28 +0530

    Upload ValidParentheses.java

---
+++ filePath = 1.8.24/ValidParentheses.java
@@ -0,0 +1,19 @@
+public class  ValidParentheses{
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            } else {
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args){
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+}
