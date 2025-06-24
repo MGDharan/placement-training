1c7e95acc79fa373bef3cd0e2b9d247c2a5cd37a
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-24 19:52:27 +0530

    Upload ListNode.java

---
+++ 1.8.24/ListNode.java
@@ -0,0 +1,17 @@
+public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
