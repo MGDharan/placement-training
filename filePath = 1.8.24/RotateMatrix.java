c77ccf5c528d5eaddff7b698f2cdc31456976fb8
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-24 19:48:19 +0530

    Upload RotateMatrix.java

---
+++ filePath = 1.8.24/RotateMatrix.java
@@ -0,0 +1,24 @@
+public class  RotateMatrix{
+    public static void rotate(int[][] matrix){
+        int n = matrix.length;
+        for(int i = 0; i < n / 2; i++){
+            for(int j = i; j < n - i - 1; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = temp;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        rotate(matrix);
+        for(int[] row : matrix){
+            for(int num : row){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
