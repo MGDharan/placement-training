792cb53aa190150d6fe2f3540d864f427cf98c31
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-24 19:48:06 +0530

    Upload HollowTriangle.java

---
+++ filePath = 1.8.24/HollowTriangle.java
@@ -0,0 +1,24 @@
+public class  HollowTriangle{
+    public static void printHollowTriangle(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+            if(i == 1 || i == n){
+                for(int k = 1; k <= i; k++){
+                    System.out.print("* ");
+                }
+            } else {
+                System.out.print("* ");
+                for(int k = 1; k <= i - 2; k++){
+                    System.out.print("  ");
+                }
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        printHollowTriangle(5);
+    }
+}
