bd939376a4cd20ad2a22098514916f85585c6d70
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-24 19:50:23 +0530

    Upload AddBinary.java

---
+++ filePath = 1.8.24/AddBinary.java
@@ -0,0 +1,21 @@
+public class  AddBinary{
+    public static String addBinary(String a, String b){
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0 || carry > 0){
+            int sum = carry;
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args){
+        String a = "11";
+        String b = "1";
+        System.out.println(addBinary(a, b));
+    }
+}
