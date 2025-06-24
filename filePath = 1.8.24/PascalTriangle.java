1eeac79bf97077a7e81c5a8c62500ff1c28517d7
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-24 19:48:08 +0530

    Upload PascalTriangle.java

---
+++ filePath = 1.8.24/PascalTriangle.java
@@ -0,0 +1,18 @@
+public class  PascalTriangle{
+    public static void printPascal(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print(combination(i,j) + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static int combination(int n, int r){
+        if(r > n) return 0;
+        if(r == 0 || r == n) return 1;
+        return combination(n-1, r-1) + combination(n-1, r);
+    }
+    public static void main(String[] args){
+        printPascal(5);
+    }
+}
