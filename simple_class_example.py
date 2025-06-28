64f7beda15cd5000d2308e98510bcb4fd6ec9960
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-28 16:22:10 +0530

    Upload simple_class_example.py

---
+++ simple_class_example.py
@@ -0,0 +1,8 @@
+# Filename:  simple_class_example.py
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        return "Woof!"
