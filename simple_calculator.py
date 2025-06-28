9c38d8674d648b91185428d0bc607afba1c10c67
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-28 16:22:15 +0530

    Upload simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,12 @@
+# Filename:  simple_calculator.py
+class Calculator:
+    def add(self, x, y):
+        return x + y
+    def subtract(self, x, y):
+        return x - y
+    def multiply(self, x, y):
+        return x * y
+    def divide(self, x, y):
+        if y == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
+        return x / y
